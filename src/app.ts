444c1a46f103efc41bcef56e220743319d1e3833
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2025-09-14 11:14:14 +0100

    refactor: remove internal cron job implementation

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from "cors";
 import config from "./config";
-import { job } from "./cron-job";
 import setupRoutes from "./routes";
 
 // Global error handlers to handle crashes
-process.on('unhandledRejection', (reason, promise) => {
+process.on("unhandledRejection", (reason, promise) => {
   console.error("Unhandled Rejection at:", promise, "reason:", reason);
   process.exit(1); // crash + restart
 });
 
-process.on('uncaughtException', (error) => {
+process.on("uncaughtException", (error) => {
   console.error("Uncaught Exception:", error);
   process.exit(1); // crash + restart
 });
@@ -38,13 +37,12 @@ app.use((req, res, next) => {
 setupRoutes(app);
 
 mongoose
-	.connect(config.mongoURI)
-	.then(() => console.log("MongoDB connected"))
-	.catch((err) => console.error(err));
+  .connect(config.mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 const port = config.port || 5000;
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
-	job.start();
+  console.log(`Server is running on port ${port}`);
 });
 export default app;
